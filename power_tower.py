b3b8ff13be568b916f399f6ae73b09d34a29dbe8
Ijaz Ahmed <bugtiijaz321@gmail.com>
2023-10-18 16:13:45 +0500

    Create power_tower.py

---
+++ power_tower.py
@@ -0,0 +1,31 @@
+'''
+Author: VBT
+Date: 10//18/2023 
+
+How Big is Graham's Number? (feat Ron Graham)
+https://www.youtube.com/watch?v=GuigptwlVHo
+Tower Notation using Python Recursive Functions
+Knuth’s Up-arrow Notation 
+https://hamzaalsamraee.com/blog/grahams-number
+https://mathworld.wolfram.com/KnuthUp-ArrowNotation.html
+
+'''
+
+def get_exp(a,b):
+    return a**b
+
+
+def get_tower(a, b, c):
+    '''
+    a: the base digit
+    b: number of arrows
+    c: digit after arrow(s)
+    '''
+    if b==1:
+        return (a**c)
+    else:
+        # 3**(3^4)
+        return a**get_tower(a,b-1,c)
+
+print("2^^^4 --> 2(3,4) ")
+print(get_tower(2,2,4))
